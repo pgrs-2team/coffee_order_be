ba33b24e6f12d80e89880bbd0fbc42214f0e96d4
MoonJaeGyeong <bblbblan69@gmail.com>
2024-09-08 16:59:37 +0900

    feat : Swagger-ui 추가

--- src/main/java/org/prgrms/coffee_order_be/controller/ProductController.java
+++ src/main/java/org/prgrms/coffee_order_be/controller/ProductController.java
@@ -1,5 +1,7 @@
 package org.prgrms.coffee_order_be.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.prgrms.coffee_order_be.model.dto.ProductDto;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Product" , description = "Product 관련 API 모음")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/product")
@@ -20,24 +23,28 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Operation(summary = "상품 생성")
     @PostMapping
     public ResponseEntity<ProductDto> createProduct(@RequestBody @Valid CreateProductReq req){
         ProductDto resp = productService.createProduct(req);
         return ResponseEntity.ok(resp);
     }
 
+    @Operation(summary = "상품 리스트 조회")
     @GetMapping
     public ResponseEntity<List<GetProductsRes>> getProducts(){
         List<GetProductsRes> resp = productService.getProducts();
         return ResponseEntity.ok(resp);
     }
 
+    @Operation(summary = "상품 상세 조회")
     @GetMapping("/{id}")
     public ResponseEntity<ProductDto> getProduct(@PathVariable(name = "id") UUID uuid){
         ProductDto resp = productService.getProduct(uuid);
         return ResponseEntity.ok(resp);
     }
 
+    @Operation(summary = "상품 수정")
     @PutMapping("/{id}")
     public ResponseEntity<ProductDto> updateProduct(@PathVariable(name = "id") UUID uuid,
                                                     @RequestBody @Valid UpdateProductReq req) {
@@ -45,6 +52,7 @@ public class ProductController {
         return ResponseEntity.ok(resp);
     }
 
+    @Operation(summary = "상품 삭제")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable(name = "id") UUID uuid){
         String resp = productService.deleteProduct(uuid);

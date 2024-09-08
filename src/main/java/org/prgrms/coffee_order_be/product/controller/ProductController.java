b2b057c1fe2f6cc939c0f21e03208c7f7d26261c
leebs0521 <leebs0521@gmail.com>
2024-09-09 00:51:48 +0900

    feat: 제품 삭제 기능

--- src/main/java/org/prgrms/coffee_order_be/product/controller/ProductController.java
+++ src/main/java/org/prgrms/coffee_order_be/product/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.prgrms.coffee_order_be.product.dto.ProductCreateDto;
 import org.prgrms.coffee_order_be.product.dto.ProductResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,12 @@ public class ProductController {
       @Valid @RequestBody ProductUpdateDto updateDto) {
     ProductResponseDto product = productService.updateProduct(productId, updateDto);
     return ResponseEntity.ok().body(product);
+  }
+
+  @DeleteMapping("/{productId}")
+  public ResponseEntity<?> deleteProduct(@PathVariable("productId") UUID productId) {
+    productService.deleteProduct(productId);
+
+    return ResponseEntity.noContent().build();
   }
 }

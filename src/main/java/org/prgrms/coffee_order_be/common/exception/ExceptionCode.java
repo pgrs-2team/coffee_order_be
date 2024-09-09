14d0156498f77cc110de39f8ec2bbcafc374d351
leebs0521 <leebs0521@gmail.com>
2024-09-09 12:24:43 +0900

    feat: 주문 조회 기능

--- src/main/java/org/prgrms/coffee_order_be/common/exception/ExceptionCode.java
+++ src/main/java/org/prgrms/coffee_order_be/common/exception/ExceptionCode.java
@@ -10,7 +10,10 @@ public enum ExceptionCode {
 
   // PRODUCT ERROR
   DUPLICATED_PRODUCT(400, "이미 존재하는 제품입니다."),
-  NOT_FOUND_PRODUCT(404, "존재하지 않는 제품입니다.");
+  NOT_FOUND_PRODUCT(404, "존재하지 않는 제품입니다."),
+
+  // ORDER ERROR
+  NOT_FOUND_ORDER(404, "존재하지 않는 주문입니다.");
 
   private final int code;
   private final String message;
--- src/main/java/org/prgrms/coffee_order_be/order/repository/OrderRepository.java
+++ src/main/java/org/prgrms/coffee_order_be/order/repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package org.prgrms.coffee_order_be.order.repository;
 
+import java.util.List;
 import java.util.UUID;
 import org.prgrms.coffee_order_be.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+  List<Order> findByEmail(String email);
 
 }
